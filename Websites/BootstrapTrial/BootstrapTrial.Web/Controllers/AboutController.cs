7818fb727efedd17fa3e1c4391fda0e227fb754a
jonjw19 <jonjw19@gmail.com>
2013-10-05 17:31:34 -0500

    Adding Personal Website
    Added personal website source code.

---
+++ Websites/BootstrapTrial/BootstrapTrial.Web/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BootstrapTrial.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+        public FileResult Resume()
+        {
+            return File("~/Content/Documents/Wilson, Jonathan.docx","msword","Wilson, Jonathan.docx");
+        }
+
+    }
+}
